6c8d08081e61d6eeec81275121e4265afffc13dc
fyh <3285206802@qq.com>
2023-08-28 17:52:03 +0800

    模板初始化

---
+++ src/layout/components/index.ts
@@ -0,0 +1,3 @@
+export {default as NavBer} from './navBer.vue'
+export {default as Content} from './content.vue'
+export {default as SilderBer} from './silderBer.vue'
---
+++ src/layout/layout.vue
@@ -0,0 +1,10 @@
+<template>
+    <div>
+        
+    </div>
+</template>
+<script lang="ts" setup>
+import { NavBer, Content, SilderBer } from './components'
+</script>
+<style lang="scss" scoped>
+</style>
